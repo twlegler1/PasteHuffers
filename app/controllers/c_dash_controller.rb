f6a26db56840e3c770f0d497946d71bed099ef88
kraig9@outlook.com <kwo227@tamu.edu>
2018-01-04 01:42:30 +0000

    Added comments

--- app/controllers/c_dash_controller.rb
+++ app/controllers/c_dash_controller.rb
@@ -1,22 +1,25 @@
 class CDashController < ApplicationController
+    #List all in database
     def index
     end
     
+    #New object. Called when asking for input
     def new
     end
     
+    #Creates a record in database
     def create
     end
     
+    #Updates record in database
     def update
     end
     
+    #CAREFUL! Will not be used for some
     def delete
     end
     
+    #Displays one element
     def show
-    end
-    
-    def list
     end
 end
