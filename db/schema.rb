a1e860dc578074edca2d9d7316d177c2cd658015
DESKTOP-9RHEF7F\matorin57 <matorin57@gmail.com>
2018-01-04 09:48:27 +0800

    Added Language Column to assignments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180104013045) do
+ActiveRecord::Schema.define(version: 20180104014715) do
 
   create_table "assignments", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180104013045) do
     t.integer  "course_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "language"
   end
 
   add_index "assignments", ["course_id"], name: "index_assignments_on_course_id"
