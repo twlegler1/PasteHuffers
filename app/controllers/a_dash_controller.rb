a24984015bdc703b85d0661282c05e3a47e530b1
kraig9@outlook.com <kwo227@tamu.edu>
2018-01-04 01:22:13 +0000

    aDashControl methods

--- app/controllers/a_dash_controller.rb
+++ app/controllers/a_dash_controller.rb
@@ -1,4 +1,16 @@
 class ADashController < ApplicationController
     def index
     end
+    
+    def new
+    end
+    
+    def create
+    end
+    
+    def update
+    end
+    
+    def delete
+    end
 end
